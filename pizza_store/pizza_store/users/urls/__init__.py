452b3f071680b21cd8d44ecd0520c00047815847
Buju-Flavia-30322 <126707421+Buju-Flavia-30322@users.noreply.github.com>
2024-02-07 20:10:38 +0200

    Delete pizza_store directory

--- pizza_store/pizza_store/users/urls/__init__.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-
-
-app_name='users'
-
-urlpatterns = [
-    path('',include('users.urls.account')),
-    path('activation/<str:token>/', include('users.urls.activation'))
-]
